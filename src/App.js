9e88a84834203392410201c97725e785388f4e71
Link1986 <ali.ghoudi@gmail.com>
2020-08-08 21:23:37 +0200

    ajout d'un titre dans la page d'accueil

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Timer from './components/Time/Timer';
 
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome to Time and Color</h1>
       <Timer />
     </div>
   );

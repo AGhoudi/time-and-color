c08c1fb5085927e3389bfc1276984a42410d722e
Link1986 <ali.ghoudi@gmail.com>
2020-08-16 19:27:31 +0200

    Updates

---
+++ precache-manifest.7a39291eace67bbec1ba1b4f0857a990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958ce0108349cc20b8f56ac5be1b1749",
+    "url": "/time-and-color/index.html"
+  },
+  {
+    "revision": "cdad90dcd0dbba25c42f",
+    "url": "/time-and-color/static/css/main.a1a74354.chunk.css"
+  },
+  {
+    "revision": "dba12029098da82e4559",
+    "url": "/time-and-color/static/js/2.994c6772.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/time-and-color/static/js/2.994c6772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdad90dcd0dbba25c42f",
+    "url": "/time-and-color/static/js/main.d793a9b3.chunk.js"
+  },
+  {
+    "revision": "5e4a21ce926c9462ba64",
+    "url": "/time-and-color/static/js/runtime-main.63df1ae4.js"
+  },
+  {
+    "revision": "bc05e9837fd6341b9633cf4c11140c31",
+    "url": "/time-and-color/static/media/background.bc05e983.jpg"
+  }
+]);
